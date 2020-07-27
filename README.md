c2217878fd459a3d1c3fffc3fe3d40e67999a328
unknown <chauncew1137@gmail.com>
2020-07-26 19:06:10 -0500

    Media queries over the last few days for responisveness

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Fitness-Site
+#mock-bootstrap-theme
---
+++ script.js
@@ -0,0 +1,9 @@
+function openFunction() {
+    document.querySelector('#menu').style.width ='100%';
+    document.querySelector('#openIcon').style.display = 'none';
+}
+
+function closeFunction() {
+    document.querySelector('#menu').style.width= '0px';
+    document.querySelector('#openIcon').style.display = 'block'
+}
